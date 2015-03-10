7e055dacd7a01e7b624a82f7e32443edde2760d4
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2015-03-10 14:46:45 -0400

    add passing minor third test

--- scales.js
+++ scales.js
@@ -14,5 +14,10 @@ exports.findFifth = function (root) {
 
 exports.findMinorThird = function (root) {
 
+  var rootIndex = notesFlat.indexOf(root);
+  var needsWrap = rootIndex < 8;
+  var thirdIndex = needsWrap ? rootIndex + 4 : rootIndex + 4 - 12;
+
+  return notesFlat[thirdIndex];
 
 };
