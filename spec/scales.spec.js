3304dac6ca6264092ee8d8e76753e60a23a8eff7
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2015-03-10 13:22:52 -0400

    initial version of scales test

---
+++ spec/scales.spec.js
@@ -0,0 +1,8 @@
+var calculator = require("../scales");
+
+describe("find-dominant", function () {
+  it("should find the V note from root", function () {
+    var dominant = scales.find-dominant(a);
+    expect(dominant).toBe(e);
+  });
+});
